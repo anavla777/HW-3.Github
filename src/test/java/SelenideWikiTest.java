0f32582c4ed5430877600cf73ac992946b906c53
anavla777 <w.ananenkow@gmail.com>
2024-04-24 01:29:07 +0300

    Тест на поиск JUnit5 кода в Selenide wiki

---
+++ src/test/java/SelenideWikiTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideWikiTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl="https://github.com";
+        Configuration.browserSize="3840x2160";
+    }
+
+    @Test
+    void findJunitExampleOnWiki() {
+        open("/selenide/selenide");
+
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
+        $("li.wiki-more-pages a[href='/selenide/selenide/wiki/SoftAssertions']")
+                .shouldHave(text("SoftAssertions")).click();
+
+        $("#wiki-body").shouldHave(text("""
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+                @Test
+                void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                }
+            }""")).shouldBe(visible);
+    }
+}
